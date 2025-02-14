13e5d4ac7a08ce9c89e8341187de8bef909b7763
Dani Biró <danibiro93@gmail.com>
2025-02-14 16:55:33 +0200

    Add tests

---
+++ tests/test.rs
@@ -0,0 +1,64 @@
+use codec::codec::encoder::EncoderSettings;
+use helpers::{encode_decode, gen_test_signal, TEST_SAMPLE_RATE};
+
+extern crate codec;
+
+mod helpers;
+
+#[test]
+fn test_sample_len() {
+    for channels in [1, 2, 3] {
+        let frame_size: i32 = 100;
+        for mul in [0, 1, 2, 3, 100] {
+            let start = ((mul * frame_size) - 2).max(0);
+            for sample_len in start..(mul * frame_size + 2) {
+                println!("Testing channels={} sample_len={}", channels, sample_len);
+                let input = gen_test_signal(channels, sample_len as usize);
+                let output = encode_decode(
+                    &input,
+                    TEST_SAMPLE_RATE,
+                    channels as u32,
+                    EncoderSettings {
+                        scale_factor_bits: 4,
+                        ..Default::default()
+                    },
+                );
+                assert_eq!(input.len(), output.decoded.len());
+                let quality = helpers::get_audio_quality(&input, &output.decoded);
+                println!("Quality: {:?}", quality);
+                // assert!(quality.psnr < -18.0);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parameters() {
+    for channels in [1, 2, 3] {
+        let input = gen_test_signal(channels, TEST_SAMPLE_RATE as usize);
+        for frame_size in [5, 10, 20] {
+            for scale_factor_bits in 3..=5 {
+                for residual_bits in 1..=8 {
+                    println!(
+                        "Testing frame_size={} channels={} scale_factor_bits={} residual_bits={}",
+                        frame_size, channels, scale_factor_bits, residual_bits
+                    );
+                    let output = encode_decode(
+                        &input,
+                        TEST_SAMPLE_RATE,
+                        channels as u32,
+                        EncoderSettings {
+                            residual_bits: residual_bits as f32,
+                            scale_factor_bits,
+                            ..Default::default()
+                        },
+                    );
+                    assert_eq!(input.len(), output.decoded.len());
+                    let quality = helpers::get_audio_quality(&input, &output.decoded);
+                    println!("Quality: {:?}", quality);
+                    assert!(quality.psnr < -20.0);
+                }
+            }
+        }
+    }
+}
